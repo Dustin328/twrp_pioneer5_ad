af26521ec750e882bbe7484429365b6989ef1180
TWRP Builder <twrp-builder@example.com>
2026-08-19 19:45:50 -0500

    Optimize recovery image size: Disable extra languages and heavy features

--- device/sstar/pioneer5_ad/BoardConfig.mk
+++ device/sstar/pioneer5_ad/BoardConfig.mk
@@ -75,7 +75,7 @@ BOARD_SUPPORTS_VBOOT := true
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := true
+TW_EXTRA_LANGUAGES := false
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/backlight/brightness"
@@ -84,6 +84,9 @@ TW_DEFAULT_BRIGHTNESS := 162
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_INCLUDE_CRYPTO := true
 TW_USE_TOOLBOX := true
+TW_EXCLUDE_APPLYPATCH := true
+TW_EXCLUDE_ENCRYPTED_BACKUPS := true
+TW_NO_EXFAT_FUSE := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
